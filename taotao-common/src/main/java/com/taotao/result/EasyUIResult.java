461e6246320f99bd16486550a11f135c36811b82
wangzhengTech <1336731039@qq.com>
2017-12-27 12:50:50 +0800

    2222222

--- taotao-common/src/main/java/com/taotao/result/EasyUIResult.java
+++ taotao-common/src/main/java/com/taotao/result/EasyUIResult.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class EasyUIResult implements Serializable {
 
 	/**
-	 * 11111
+	 * 11111 222222
 	 */
 	private static final long serialVersionUID = -287596517774651742L;
 
